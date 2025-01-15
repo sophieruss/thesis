9af565c4bf4c91acdff00ffa9af86472e54cef85
sophieruss <soruss@calpoly.edu>
2025-01-15 15:44:25 -0800

    memory/rest of commands

--- commands.py
+++ commands.py
@@ -24,3 +24,42 @@ class Bgtz:
     def __init__(self, src, n):
         self.src = src
         self.n = n
+
+class Sw:
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+    
+class Lw_U:
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+        
+class Lw_T:
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+        
+class Get:
+    def __init__(self, dest):
+        self.dest = dest
+
+class Put:
+    def __init__(self, src):
+        self.src = src
+        
+class trustedMode:
+    def __init__(self):
+        pass
+    
+class untrustedMode:
+    def __init__(self):
+        pass
+    
+class alert:
+    def __init__(self):
+        pass
+    
+class returnn:
+    def __init__(self):
+        pass
--- host.py
+++ host.py
@@ -4,65 +4,184 @@ r = [0] * 32    # registers r -> X
 R = [0] * 32    # register file r -> Z
 M = {}          # memory
 S = 0           # mode bit
-program = {}
-
-from commands import Add, Addi, Sub, Jump, Bgtz
+program = {}    # Z -> commands
+getQ = []
+trace = []       
+from commands import Add, Addi, Sub, Jump, Bgtz, Sw, Lw_U, Lw_T, Get, Put, trustedMode, untrustedMode, alert, returnn
 
 
 def fun_add(dest, src, temp):
-    global pc, R, S, program
+    global pc, R, S, program, trace
     cmd = Add(dest, src, temp)
     program[pc] = cmd
     n_0 = R[src]
     n_1 = R[temp]
     n_2 = n_0 + n_1
     if S == 1:
+        trace = [program[pc], n_0, n_1, n_2]
+        
+        # ok these two lines are allegedly after trace logic - ask!
         R[dest] = n_2
         pc += 1
+    else:
+        trace = []
+    
 
 def fun_addi(dest, src, n_1):
-    global pc, R, S, program
+    global pc, R, S, program, trace
     cmd = Addi(dest, src, n_1)
     program[pc] = cmd
     n_0 = R[src]
     n_2 = n_0 + n_1
     if S == 1:
+        trace = [program[pc], n_0, n_2]
         R[dest] = n_2
         pc += 1
+    else:
+        trace = []
 
 def fun_sub(dest, src, temp):
-    global pc, R, S, program
+    global pc, R, S, program, trace
     cmd = Sub(dest, src, temp)
     program[pc] = cmd
     n_0 = R[src]
     n_1 = R[temp]
     n_2 = n_0 - n_1
     if S == 1:
+        trace = [program[pc], n_0, n_1, n_2]
         R[dest] = n_2
         pc += 1
+    else:
+        trace = []
+        
+        
 
 def fun_jump(n):
-    global pc, program, S
+    global pc, program, S, trace
     cmd = Jump(n)
     program[pc] = cmd
     if S == 1:
+        trace = [program[pc], n]
         pc = n
 
+    else:
+        trace = []
+
 def fun_bgtz_g(src, n):
-    global pc, R, S, program
+    global pc, R, S, program, trace
     cmd = Bgtz(src, n)
     program[pc] = cmd
     n_1 = R[src]
     if S == 1 and n_1 > 0:
+        trace = [program[pc], n_1, n]
         pc = n
 
+    else:
+        trace = []
+
 def fun_bgtz_l(src, n):
-    global pc, R, S, program
+    global pc, R, S, program, trace
     cmd = Bgtz(src, n)
     program[pc] = cmd
     n_1 = R[src]
     if S == 1 and n_1 <= 0:
+        trace = [program[pc], n_1, pc+1]
         pc += 1
+        
+def fun_load_unt(src, dest):
+    global pc, R, S, program, trace
+    cmd = Lw_U(src, dest)
+    program[pc] = cmd
+    n_0 = R[src]
+    n_1 = M[n_0]
+    if S == 1:
+        trace = [program[pc], n_1, pc+1]
+        R[dest] = n_1
+        pc += 1
+    else:
+        trace = []
+
+def fun_load(src, dest):
+    global pc, R, S, program, trace
+    cmd = Lw_T(src, dest)
+    program[pc] = cmd
+    n_0 = R[src]
+    n_1 = M[n_0]
+    if S == 1:
+        trace = [program[pc], n_0, n_1]
+        R[dest] = n_1
+        pc += 1
+    else:
+        trace = []
+        
+def fun_store(src, dest):
+    global pc, R, S, program, trace
+    cmd = Sw(src, dest)
+    program[pc] = cmd
+    n_0 = R[dest]
+    n_1 = R[src]
+    if S == 1:
+        trace = [program[pc], n_0, n_1]
+        M[n_0] = n_1
+        pc += 1
+    else:
+        trace = []
+        
+def fun_get(dest):
+    global pc, R, S, program, trace
+    cmd = Get(dest)
+    program[pc] = cmd
+    
+    n = -1              # how to get n????
+    getQ_0 = getQ + [n]   # does :: mean add it to queue?
+    
+    if S == 1:
+        trace = [program[pc], n]
+        R[dest] = n
+        getQ = getQ_0
+        pc += 1
+    else:
+        trace = []
+        
+def fun_put(src):
+    global pc, R, S, program, trace
+    cmd = Put(src)
+    program[pc] = cmd
+    
+    n = R[src]
+    if S == 1:
+        trace = [program[pc], n]
+        pc += 1
+    else:
+        trace = []
+
+def fun_enable():
+    global S, program
+    cmd = trustedMode()
+    program[pc] = cmd
+    S = 1
+    pc += 1
+
+def fun_disable():
+    global S, program
+    cmd = untrustedMode()
+    program[pc] = cmd
+    S = 0
+    pc += 1
+    
+def fun_alert():
+    global program, trace
+    cmd = alert()
+    program[pc] = cmd
+    if S == 1:
+        trace = [program[pc]]
+
+def fun_returnn():
+    global program, trace
+    cmd = returnn()
+    program[pc] = cmd
+    if S == 1:
+        trace = [program[pc]] 
 
 
 def print_program():
@@ -85,9 +204,15 @@ def main():
     print_program()
 
     fun_add(1, 2, 3)
+    fun_addi(4, 1, 5)
+    # fun_jump(0)
+    fun_sub(1, 4, 3)
+    
     
     print_program()
 
     return 0
 
 main()
+# 4257
+# how do I deal with trace values? It sends from host to sentry, but how?
